75a64d1d92b27c45c70126163169220c0febb908
Giampier-pixel <usuarioae87@gmail.com>
2025-05-26 12:25:07 -0500

    listo

--- controller/tipo/controlador_registro_tipo.php
+++ controller/tipo/controlador_registro_tipo.php
@@ -1,6 +1,6 @@
 <?php
-    require "../../model/model_area.php";
-    $MU = new Modelo_Tipo();//instanciando 
+    require '../../model/model_tipo.php';
+    $MU = new Modelo_Tipo();//Instaciamos
     $tipo= strtoupper(htmlspecialchars($_POST["tipo"],ENT_QUOTES,"UTF-8"));
     $consulta= $MU-> Registrar_Tipo($tipo);
     echo $consulta;
